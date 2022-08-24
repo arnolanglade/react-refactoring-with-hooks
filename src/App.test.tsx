498cb1b3a1a378e1a85a7d309d72db981bb8da74
arnaud <arnaud.langlade@gmail.com>
2022-08-24 22:49:08 +0200

    feat: bad code to refactor

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, within} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
+describe('todo list', () => {
+    test('I should be able to mark a task as done', () => {
+        render(<App/>);
+
+        const task = within(screen.getByTestId('task-1'));
+
+        userEvent.click(task.getByRole('checkbox'));
+
+        expect(task.getByText('done')).toBeInTheDocument()
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, {ReactNode, useEffect, useState} from 'react';
+import {List, ListItem, ListItemText, ListSubheader, Switch} from "@mui/material";
+
+type Task = {
+  id: number;
+  label: ReactNode;
+  done: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    setTasks([
+      {id: 1, label: 'task 1', done: true},
+      {id: 2, label: 'task 2', done: true},
+      {id: 3, label: 'task 3', done: false},
+      {id: 4, label: 'task 4', done: true},
+      {id: 5, label: 'task 5', done: true},
+    ]);
+  }, []);
+
+  const onTaskDoneButtonClick = (taskId: number) => () => {
+    setTasks(tasks.map((todo: Task) => {
+      if (todo.id === taskId) {
+        return {...todo, label: <><del>{todo.label}</del> done</>,  done: false}
+      }
+      return todo;
+    }))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <List
+          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', margin: 'auto' }}
+          subheader={<ListSubheader>Todo list</ListSubheader>}
+      >
+        {
+          tasks.map((todo: Task) => <ListItem key={todo.id} data-testid={`task-${todo.id}`}>
+            <ListItemText primary={todo.label} />
+            <Switch
+                edge="end"
+                data-testid={todo.label}
+                onChange={onTaskDoneButtonClick(todo.id)}
+                checked={todo.done}
+            />
+          </ListItem>
+          )
+        }
+      </List>
   );
 }
 
