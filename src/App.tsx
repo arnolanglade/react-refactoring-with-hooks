7e9506d4257863ba6ca5912602f99fb54c6056ef
arnaud <arnaud.langlade@gmail.com>
2022-08-24 22:49:08 +0200

    feat: bad code to refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
+import React, {ReactNode, useEffect, useState} from 'react';
+import {List, ListItem, ListItemText, ListSubheader, Switch} from "@mui/material";
+
+type Task = {
+    id: number;
+    label: ReactNode;
+    done: boolean;
+}
+
+const findAllTasks = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            () => resolve([
+                {id: 1, label: 'task 1', done: true},
+                {id: 2, label: 'task 2', done: true},
+                {id: 3, label: 'task 3', done: false},
+                {id: 4, label: 'task 4', done: true},
+                {id: 5, label: 'task 5', done: true},
+            ]),
+            5000
+        );
+    });
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    useEffect(() => {
+        (async () => await findAllTasks())();
+    }, []);
+
+    const onTaskDoneButtonClick = (taskId: number) => () => {
+        setTasks(tasks.map((todo: Task) => {
+            if (todo.id === taskId) {
+                return {...todo,
+                    label: <>
+                        <del>{todo.label}</del>
+                        done</>,
+                    done: false
+                }
+            }
+            return todo;
+        }))
+    };
+
+    return (
+        <List
+            sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper', margin: 'auto'}}
+            subheader={<ListSubheader>Todo list</ListSubheader>}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            {
+                tasks.map((todo: Task) => <ListItem key={todo.id}>
+                        <ListItemText primary={todo.label}/>
+                        <Switch
+                            edge="end"
+                            onChange={onTaskDoneButtonClick(todo.id)}
+                            checked={todo.done}
+                        />
+                    </ListItem>
+                )
+            }
+        </List>
+    );
 }
 
 export default App;
